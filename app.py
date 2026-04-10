b7e196f100cabe5b3b2e8323e5df88486c5c2c22
C <s6cnwink@uni-bonn.de>
2026-04-10 12:39:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,19 +12,23 @@ from langchain_chroma import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from huggingface_hub import snapshot_download
+import shutil
 
 # -------- Config (can be overridden via Space "Variables") --------
 PERSIST_DIR = os.getenv("PERSIST_DIR", "./chroma_langchain")     # path to your committed Chroma index
 EMB_MODEL   = os.getenv("EMB_MODEL", "jinaai/jina-embeddings-v3")
 TOPK_DEF    = int(os.getenv("TOPK", "5"))
 
-if not os.path.exists(PERSIST_DIR) or not os.listdir(PERSIST_DIR):
-    print("Downloading Chroma index from HuggingFace...")
-    snapshot_download(
-        repo_id="cwinkler/bgh-chroma-index",
-        repo_type="dataset",
-        local_dir=PERSIST_DIR,
-    )
+# Alten Ordner löschen und neu laden
+if os.path.exists(PERSIST_DIR):
+    shutil.rmtree(PERSIST_DIR)
+
+print("Downloading Chroma index from HuggingFace...")
+snapshot_download(
+    repo_id="cwinkler/bgh-chroma-index",
+    repo_type="dataset",
+    local_dir=PERSIST_DIR,
+)
 
 # Embedding function for query text — must match the model used to build the index
 EMBEDDINGS = HuggingFaceEmbeddings(
